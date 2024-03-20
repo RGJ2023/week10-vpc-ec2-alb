da01c99c61c1f0d8c12982df78dddffca87f5ed2
RGJ2023 <rasabea@gmail.com>
2024-03-19 20:53:23 -0400

    new code

---
+++ backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "week-10-ra-terraform"
+    key            = "week-10/terraform.tfstate"
+    region         = "us-east-1"
+    encrypt        = true
+    dynamodb_table = "state-log"
+  }
+}
