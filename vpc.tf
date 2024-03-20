3e6242e2768fbc001b24dfc9c850125ffba31265
RGJ2023 <rasabea@gmail.com>
2024-03-19 22:08:12 -0400

    terraform

--- vpc.tf
+++ vpc.tf
@@ -24,7 +24,7 @@ resource "aws_subnet" "public1" {
 
 resource "aws_subnet" "public2" {
   availability_zone = "us-east-1b"
-  cidr_block = "192.168.1.0/24"
+  cidr_block = "192.168.2.0/24"
   vpc_id = aws_vpc.vpc1.id
   map_public_ip_on_launch = true
   tags={
@@ -103,52 +103,3 @@ resource "aws_route_table_association" "rta4" {
   route_table_id = aws_route_table.rtpublic.id
 }
 
-# sg.tf
-
-resource "aws_security_group" "sg1" {
-    name = "terraform-sg"
-    description = "Allow ssh and httpd"
-    
-    
-    ingress {
-        description = "allow http"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        #cidr_blocks = ["0.0.0.0/0"]
-        security_groups = [ aws_security_group.sg2.name ]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  tags= {
-    env = "Dev"
-  }
-
-  
-}
-resource "aws_security_group" "sg2" {
-    name = "terraform-sg-lb"
-    description = "Allow ssh and httpd"
-    
-    
-    ingress {
-        description = "allow http"
-        from_port = 80
-        to_port = 80
-        protocol = "tcp"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-    egress {
-        from_port = 0
-        to_port = 0
-        protocol = "-1"
-        cidr_blocks = ["0.0.0.0/0"]
-    }
-  tags= {
-    env = "Dev"
-  } 
-}
